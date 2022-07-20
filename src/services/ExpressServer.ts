4a117f5584cc7cfa494ea348d934c984cc47e7bb
mcesarpl <mcesarpl123@gmail.com>
2022-07-19 23:47:31 -0300

    feat: express and dotenv added

---
+++ src/services/ExpressServer.ts
@@ -0,0 +1,49 @@
+import express from 'express';
+import rateLimit from 'express-rate-limit';
+import cors from 'cors';
+import { StatusCodes, ReasonPhrases } from 'http-status-codes';
+import routes from '../routes/index';
+
+export class ExpressApp {
+  private app = express();
+
+  midddlewares() {
+    const whitelist = ['https://localhost'];
+
+    const corsOptions: cors.CorsOptions = {
+      origin: whitelist,
+    };
+
+    const apiLimiter = rateLimit({
+      windowMs: 1 * 60 * 1000, // 1 minutes
+      max: 120,
+      statusCode: StatusCodes.TOO_MANY_REQUESTS,
+      message: ReasonPhrases.TOO_MANY_REQUESTS,
+    });
+
+    this.app.use(cors(corsOptions));
+    this.app.use(express.json());
+
+    this.app.use((req, res, next) => {
+      if (req.header('authorization') === process.env.AUTHORIZATION_TOKEN) {
+        next();
+      } else {
+        res.status(StatusCodes.UNAUTHORIZED).send({
+          message: ReasonPhrases.UNAUTHORIZED,
+        });
+      }
+    });
+
+    this.app.use('/', apiLimiter);
+  }
+
+  routes() {
+    this.app.use(routes);
+  }
+
+  async init() {
+    this.midddlewares();
+    this.routes();
+    this.app.listen(process.env.PORT);
+  }
+}
---
+++ src/services/index.ts
@@ -0,0 +1,2 @@
+export * from './ExpressServer';
+export * from './InitEnv';
