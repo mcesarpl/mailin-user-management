3dfb930c45c44e032e032c7cb3c6cf7452de066a
Mateus Landim <mateus@MBP-de-Mateus.lan>
2023-06-01 13:41:04 +0100

    WIP

---
+++ src/adapters/PostgresClient.ts
@@ -0,0 +1,38 @@
+import { IDatabase } from '@src/interfaces/IDatabase';
+import { Model } from 'mongoose';
+
+export class PostgresClient<T> implements IDatabase<T, T | null> {
+  constructor(private model: Model<T>) {}
+
+  async create(instance: T & { _id: string }) {
+    await this.model.create<T>(instance);
+    return instance;
+  }
+
+  async updateOne(instance: T & { _id: string }) {
+    await this.model.updateOne<T>({ _id: instance._id }, { ...instance });
+    return instance;
+  }
+
+  async findOne(instanceId: string) {
+    const instance = await this.model
+      .findOne<T>({
+        _id: instanceId,
+      })
+      .lean();
+
+    if (!instance) {
+      return null;
+    }
+
+    return instance as T;
+  }
+
+  async find(params: object): Promise<T[]> {
+    return await this.model.find<T>({ ...params }).lean();
+  }
+
+  async deleteOne(instanceId: string) {
+    await this.model.deleteOne({ _id: instanceId });
+  }
+}
